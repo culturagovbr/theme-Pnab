6148310aab0b444f30043de90a5917ed142b4059
Nathaly Magalhaes <nathaly.magalhaes19@hotmail.com>
2026-02-25 23:14:57 -0300

    feat: atualiza configurações e ícones do sistema

---
+++ components/opportunity-category/template.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->layout = 'entity';
+
+$this->import('
+    opportunity-category-list 
+    mc-card
+');
+?>
+<div class="opportunity-category">
+</div>
--- layouts/parts/header-menu-opportunity-link.php
+++ layouts/parts/header-menu-opportunity-link.php
@@ -8,11 +8,13 @@ use MapasCulturais\i;
 ?>
 <li v-if="global.enabledEntities.opportunities">
     <a href="https://cultbr.cultura.gov.br/transparencia" class="mc-header-menu--item opportunity">
+        <img src="<?= $app->view->asset('img/home/header/prancheta-2.jpg', false) ?>" alt="Ícone" class="menu-icon" width="24" height="24" />  
         <p class="label"> <?php i::_e('Portal CultBR') ?> </p>
     </a>
 </li>
 <li v-if="global.enabledEntities.agents">
     <a href="https://cultbr.cultura.gov.br/entrar" class="mc-header-menu--item agent">
+        <img src="<?= $app->view->asset('img/home/header/prancheta-2.jpg', false) ?>" alt="Ícone" class="menu-icon" width="24" height="24" />  
         <p class="label"> <?php i::_e('Rede CultBR') ?> </p>
     </a>
 </li>
