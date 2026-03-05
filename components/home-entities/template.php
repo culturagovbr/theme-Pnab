b5f8414bce4ad530e170ebf262d68bbdacfad23d
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-03-05 16:46:52 -0300

    ajustando cards

--- components/home-entities/template.php
+++ components/home-entities/template.php
@@ -10,12 +10,19 @@ $this->import('mc-link');
 
 $entities = [
     'opportunities' => [
-        'image' => 'img/cards/oportunidades_bg.jpg',
+        'image' => 'img/cards/oportunidades_bg.png',
         'title' => '',
         'route' => 'search/opportunities',
         'viewAll' => 'Acesse aqui',
         'link' => 'https://cultbr.cultura.gov.br/transparencia',
         'description' => 'Aqui você encontra atividades e chamadas com inscrições abertas, como editais, intercâmbios, residências artísticas e outras oportunidades disponíveis. Você também pode criar e divulgar novas oportunidades para outros agentes da Rede.',
+    ],
+    'signup' => [
+        'image' => 'img/cards/cadastro_bg.png',
+        'title' => '',
+        'route' => 'autenticacao',
+        'viewAll' => 'Fazer Cadastro',
+        'description' => 'O <strong>Cult.br Editais</strong> é uma plataforma gratuita. Gestores culturais divulgam editais da <strong>Política Nacional Aldir Blanc</strong> e agentes culturais submetem suas propostas. Aqui você pode realizar a inscrição, seleção, monitoramento e prestação de informações sobre projetos. Acesse e faça parte da <strong>maior política pública de fomento cultural da história!</strong>',
     ],
 ];
 ?>
@@ -26,13 +33,13 @@ $entities = [
     <div class="home-entities__content--cards">
       <?php foreach ($entities as $key => $entity): ?>
         <?php $imageUrl = $app->view->asset($entity['image'], false); ?>
-        <div v-if="global.enabledEntities.<?= $key ?>" class="card">
+        <div v-if="<?= $key === 'signup' ? 'true' : "global.enabledEntities.$key" ?>" class="card">
           <div class="card__image" style="background-image: url('<?= $imageUrl ?>');">
             <h3><?= i::__($entity['title']) ?></h3>
           </div>
           <div class="card__body">
             <p><?= i::__($entity['description']) ?></p>
-            <a href="<?= $entity['link'] ?>" target="_blank">
+            <a href="<?= $entity['link'] ?? $app->createUrl($entity['route']) ?>" target="_blank">
               <?= i::__($entity['viewAll']) ?>
             </a>
           </div>
