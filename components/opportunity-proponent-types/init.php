47f846c1f936dfb0201112136ba80322ff22b6f9
Victor Ferreira <vitfera@hotmail.com>
2026-08-07 13:07:45 -0300

    feat: permite configurar vínculo coletivo para admins

--- components/opportunity-proponent-types/init.php
+++ components/opportunity-proponent-types/init.php
@@ -1,3 +1,6 @@
 <?php
 
+use AldirBlanc\Services\UserAccessService;
+
 $this->jsObject['config']['opportunityProponentTypes'] = $app->config['registration.proponentTypesToAgentsMap'];
+$this->jsObject['config']['opportunityProponentTypesCanConfigureAgentRelation'] = UserAccessService::isAdmin();
