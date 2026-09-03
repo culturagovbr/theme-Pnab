41f5a5fdac271abea1045c4b28c3e81eff3b2dec
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-09-03 06:21:37 -0300

    feat: avisa o gestor ao escolher ente federado sem dados do PAR

--- components/federative-entity-selector/template.php
+++ components/federative-entity-selector/template.php
@@ -8,6 +8,7 @@
 use MapasCulturais\i;
 
 $this->import('
+    mc-modal
     mc-icon
 ');
 ?>
@@ -75,4 +76,26 @@ $this->import('
             </div>
         </div>
     </template>
+
+    <mc-modal
+        ref="parWarningModal"
+        title="<?= i::esc_attr__('Dados do PAR em análise') ?>"
+        classes="federative-entity-selector__par-warning"
+        @close="discardPendingEntity">
+        <template #button>
+            <!-- Botão vazio para não renderizar o botão padrão do modal -->
+        </template>
+        <template #default>
+            <p><?php i::_e('O PAR deste ente federado está em análise, então não é possível criar novas oportunidades por ele.') ?></p>
+            <p><?php i::_e('As oportunidades que você já criou continuam disponíveis para edição normalmente.') ?></p>
+        </template>
+        <template #actions="modal">
+            <button class="button button--text" @click="modal.close()">
+                <?php i::_e('Escolher outro ente') ?>
+            </button>
+            <button class="button button--primary" @click="confirmPendingEntity(modal)">
+                <?php i::_e('Continuar mesmo assim') ?>
+            </button>
+        </template>
+    </mc-modal>
 </div>
--- components/federative-entity-selector/texts.php
+++ components/federative-entity-selector/texts.php
@@ -8,4 +8,8 @@ return [
     'Selecione o ente federado que deseja utilizar:' => i::__('Selecione o ente federado que deseja utilizar:'),
     'Confirmar Seleção' => i::__('Confirmar Seleção'),
     'Dados do PAR em análise' => i::__('Dados do PAR em análise'),
+    'O PAR deste ente federado está em análise, então não é possível criar novas oportunidades por ele.' => i::__('O PAR deste ente federado está em análise, então não é possível criar novas oportunidades por ele.'),
+    'As oportunidades que você já criou continuam disponíveis para edição normalmente.' => i::__('As oportunidades que você já criou continuam disponíveis para edição normalmente.'),
+    'Escolher outro ente' => i::__('Escolher outro ente'),
+    'Continuar mesmo assim' => i::__('Continuar mesmo assim'),
 ];
